719b2937ad338837f229eb904538833b3280a670
Rustam Gasanov <rustamagasanov@gmail.com>
2015-09-21 23:33:49 +0200

    Completed docs for table formatter

--- mix_projects/issues/lib/issues/table_formatter.ex
+++ mix_projects/issues/lib/issues/table_formatter.ex
@@ -26,7 +26,6 @@ defmodule Issues.TableFormatter do
   list of columns to extract
 
   ## Example
-
     iex> list = [Enum.into([{"a", "1"},{"b", "2"},{"c", "3"}], HashDict.new),
     ...>         Enum.into([{"a", "4"},{"b", "5"},{"c", "6"}], HashDict.new)]
     iex> Issues.TableFormatter.split_into_columns(list, [ "a", "b", "c" ])
@@ -43,7 +42,6 @@ defmodule Issues.TableFormatter do
   Return a binary (string) version of our parameter.
 
   ## Examples
-
       iex> Issues.TableFormatter.printable("a")
       "a"
       iex> Issues.TableFormatter.printable(99)
@@ -53,18 +51,49 @@ defmodule Issues.TableFormatter do
   def printable(str) when is_binary(str), do: str
   def printable(str), do: to_string(str)
 
+  @doc """
+  Given a list containing sublists, where each sublist contains the data for
+  a column, return a list containing the maximum width of each column
+
+  ## Example
+      iex> data = [ [ "cat", "wombat", "elk"], ["mongoose", "ant", "gnu"]]
+      iex> Issues.TableFormatter.widths_of(data)
+      [ 6, 8 ]
+  """
   def widths_of(columns) do
     for column <- columns, do: column |> map(&String.length/1) |> max
   end
 
+  @doc """
+  Return a format string that hard codes the widths of a set of columns.
+  We put `" | "` between each column.
+
+  ## Example
+      iex> widths = [5,6,99]
+      iex> Issues.TableFormatter.format_for(widths)
+      "~-5s | ~-6s | ~-99s~n"
+  """
   def format_for(column_widths) do
     "| " <> map_join(column_widths, " | ", fn width -> "~-#{width}s" end) <> " |~n"
   end
 
+  @doc """
+  Generate the line that goes below the column headings. It is a string of
+  hyphens, with + signs where the vertical bar between the columns goes.
+
+  ## Example
+        iex> widths = [5,6,9]
+        iex> Issues.TableFormatter.separator(widths)
+        "------+--------+----------"
+  """
   def separator(column_widths) do
     "|-" <> map_join(column_widths, "-+-", fn width -> List.duplicate("-", width) end) <> "-|"
   end
 
+  @doc """
+  Given a list containing rows of data, a list containing the header selectors,
+  and a format string, write the extracted data under control of the format string.
+  """
   def puts_in_columns(data_by_columns, format) do
     data_by_columns
     |> List.zip
