d1bc84468742600df4771e69d55afed59db4386f
Rustam Gasanov <rustamagasanov@gmail.com>
2015-11-06 20:55:09 -0500

    Added sigils

---
+++ sigils.exs
@@ -0,0 +1,6 @@
+IO.puts "~C A character list with no escaping or interpolation"
+IO.puts "~c A character list, escaped and interpolated just like a single-quoted string ~R A regular expression with no escaping or interpolation"
+IO.puts "~r A regular expression, escaped and interpolated"
+IO.puts "~S A string with no escaping or interpolation"
+IO.puts "~s A string, escaped and interpolated just like a double-quoted string"
+IO.puts "~W A list of whitespace-delimited words, with no escaping or interpolation ~w A list of whitespace-delimited words, with escaping and interpolation"
