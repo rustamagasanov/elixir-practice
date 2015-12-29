20a89fd62eb2eb1f1cedb27d67df8cc99964656c
Rustam Gasanov <rustamagasanov@gmail.com>
2015-12-29 18:34:53 -0500

    Updated nodes usage

--- nodes_usage.exs
+++ nodes_usage.exs
@@ -1,7 +1,8 @@
-# iex --sname node_one            - runs new named node(Erlang VM)
-# Node.self                       - outputs current node_name
-# Node.connect :"other_node@host" - connects to other node
+# iex --sname node_one                     - runs new named node(Erlang VM)
+# Node.self                                - outputs current node_name
+# Node.connect :"other_node@host"          - connects to other node
 
 # func = fn -> IO.inspect Node.self end
-# spawn(func)                     - spawns func on current node
-# Node.spawn(Node.self, func)     - same call run explicitly
+# spawn(func)                              - spawns func on current node
+# Node.spawn(Node.self, func)              - same call run explicitly
+# Node.spawn(:"other_node@host", func)     - spawns func on other node, outputs result on cur node
