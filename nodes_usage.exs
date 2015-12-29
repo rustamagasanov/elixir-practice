5a63100df169208d0b2ecea7355ca74a692bb190
Rustam Gasanov <rustamagasanov@gmail.com>
2015-12-29 18:32:02 -0500

    Added nodes usage example

---
+++ nodes_usage.exs
@@ -0,0 +1,3 @@
+# iex --sname node_one            - runs new named node(Erlang VM)
+# Node.self                       - outputs current node_name
+# Node.connect :"other_node@host" - connects to other node
