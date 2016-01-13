0762776b8b674cf4aa473a966720291eff8ce4a4
Rustam Gasanov <rustamagasanov@gmail.com>
2016-01-13 12:45:05 +0100

    Added monitor hw1

---
+++ monitor_exercise_1.exs
@@ -0,0 +1,30 @@
+defmodule MonitorExercise1 do
+  import :timer, only: [ sleep: 1 ]
+
+  def sad_function(pid) do
+    send pid, { :ok, "message 1" }
+    send pid, { :ok, "message 2" }
+    send pid, { :ok, "message 3" }
+    exit(:boom)
+  end
+
+  def run do
+    Process.flag(:trap_exit, true)
+    spawn_monitor(MonitorExercise1, :sad_function, [self])
+    sleep 500
+    receive_messages
+  end
+
+  def receive_messages do
+    receive do
+      msg ->
+        IO.puts "MESSAGE RECEIVED: #{inspect msg}"
+    after 1000 ->
+        IO.puts "Nothing happened as far as I am concerned"
+        exit(:ok)
+    end
+    receive_messages
+  end
+end
+
+MonitorExercise1.run
